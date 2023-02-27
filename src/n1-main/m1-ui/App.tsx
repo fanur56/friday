ef634591b5e59284ca34bda6e384c0314196a5ca
fanur56 <ilasovf@gmail.com>
2023-02-27 23:34:39 +0300

    Refactor routes

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Main} from "./Main";
 import {Test} from "../../n2-features/f0-test/Test";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {NotFoundPage} from "./NotFound/NotFoundPage";
 import {AddNewPassword} from "../../n2-features/f1-auth/a3-assword/AddNewPassword";
 import {RecoveryPassword} from "../../n2-features/f1-auth/a3-assword/RecoveryPassword";
@@ -11,11 +11,12 @@ export const App = () => {
     return (
         <div className="App">
             <Routes>
-                <Route path={"/"} element={<Main/>}/>
-                <Route path={"/test"} element={<Test/>}/>
-                <Route path={"/new-password"} element={<AddNewPassword/>}/>
-                <Route path={"/recovery-password"} element={<RecoveryPassword/>}/>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/test" element={<Test/>}/>
+                <Route path="/new-password" element={<AddNewPassword/>}/>
+                <Route path="/recovery-password" element={<RecoveryPassword/>}/>
                 <Route path={"/404"} element={<NotFoundPage/>}/>
+                <Route path={"/*"} element={<Navigate to={"/404"}/>}/>
             </Routes>
         </div>
     );
